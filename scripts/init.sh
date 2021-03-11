489979db133f18037ccc9cc3a6830c2d97e2d2cd
Dmitry Egorov <isicju@gmail.com>
2021-03-11 09:11:42 +0300

    removing docker, adding mysql and tomcat

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,15 +8,15 @@ apt install git -y
 apt install mysql-server -y
 #allowing remote access to mysql
 vim /etc/mysql/mysql.conf.d/mysqld.cnf
-# DO -> change bind-address = 0.0.0.0
+# change bind-address = 0.0.0.0
 
 #adding privileges for remote access
 mysql -u root
-# DO -> mysql.sql script
+# execute mysql.sql script
 
 #installing tomcat
 
 wget https://apache-mirror.rbc.ru/pub/apache/tomcat/tomcat-8/v8.5.63/bin/apache-tomcat-8.5.63.tar.gz
 tar -xf apache-tomcat-8.5.63.tar.gz
 cd apache-tomcat-8.5.63/conf
-vim tomcat-users.xml
+## vim tomcat-users.xml and add admin roles (tomcat.xml)
