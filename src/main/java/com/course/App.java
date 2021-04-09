2d47b8f6e186514dc4d69224c92f20e05434633f
Dmitry Egorov <isicju@gmail.com>
2021-04-09 01:30:23 +0100

    Example with serving static files

--- src/main/java/com/course/App.java
+++ src/main/java/com/course/App.java
@@ -1,7 +1,13 @@
 package com.course;
 
+import spark.Spark;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Spark.setPort(1111);
+        Spark.staticFileLocation("/public");
+        Spark.get("/rest", (req, res) -> {
+            return "hello from sparkjava.com";
+        });
     }
 }
