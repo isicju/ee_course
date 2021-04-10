b58fbaf51cc888508a5ae6c15ee34093e9481da4
Dmitry Egorov <isicju@gmail.com>
2021-04-10 15:48:41 +0100

    Example with serving static files

--- src/main/java/com/course/basics/TCPServerWithHttpRequest.java
+++ src/main/java/com/course/basics/TCPServerWithHttpRequest.java
@@ -4,12 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class TCPServerWithHttpRequest {
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(8282);
         Socket clientSocket = serverSocket.accept();
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(),  StandardCharsets.UTF_8));
         int value = 0;
         while (in.ready()) {
             value = in.read();
