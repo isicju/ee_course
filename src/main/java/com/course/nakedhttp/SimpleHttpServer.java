6dac83bd0bf8f9250c23ff3aa7aa008b6b1504b4
Dmitry Egorov <isicju@gmail.com>
2021-04-24 11:15:28 +0100

    test

--- src/main/java/com/course/nakedhttp/SimpleHttpServer.java
+++ src/main/java/com/course/nakedhttp/SimpleHttpServer.java
@@ -21,7 +21,7 @@ public class SimpleHttpServer {
   void start() throws IOException {
     System.out.println("Server has started..");
     server = HttpServer.create(new InetSocketAddress(PORT), 0);
-    server.createContext("", new StaticFileHandler());
+    server.createContext("/", new StaticFileHandler());
     server.start();
   }
 
